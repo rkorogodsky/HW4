57c825be14a89fae53b8d42b4a29308619e5334e
rkorogodsky <rk798598@wcupa.edu>
2014-11-14 14:10:35 -0500

    Revert "mouse and fixed alignment"
    This reverts commit 16d90da994ef81e02f43570a373d0d48de813bc4.

--- blockbreak.js
+++ blockbreak.js
@@ -3,11 +3,10 @@ $(function() {
                      .include('Input,Sprites,Scenes')
                      .setup();
 
-  // Q.input.keyboardControls();
-  Q.input.mouseControls();
-  /*Q.input.touchControls({ 
-            controls:  [ ['left','mouseX' ],[],[],[],['right','mouseX' ] ]
-  });*/
+  Q.input.keyboardControls();
+  Q.input.touchControls({ 
+            controls:  [ ['left','<' ],[],[],[],['right','>' ] ]
+  });
 
   Q.Sprite.extend("Paddle", {     // extend Sprite class to create Q.Paddle subclass
     init: function(p) {
@@ -16,34 +15,27 @@ $(function() {
         speed: 200,
         x: 0,
       });
-      this.p.x = Q.width - this.p.w;
+      this.p.x = Q.width/2 - this.p.w/2;
       this.p.y = Q.height - this.p.h;
       if(Q.input.keypad.size) {
         this.p.y -= Q.input.keypad.size + this.p.h;
       }
     },
 
-    step: function(dt) 
-		{
-		  this.p.x = Q.inputs['mouseX'];
-		  //{ 
-		  //  this.p.x -= dt * this.p.speed;
-		  //} else if(Q.inputs['mouseX']) {
-		   // this.p.x += dt * this.p.speed;
-		  //}
-					  if(this.p.x < this.p.w/2) 
-						  { 
-							this.p.x = this.p.w/2;
-						  } 
-						  else if(this.p.x > Q.width - this.p.w/2) 
-						  { 
-							this.p.x = Q.width - this.p.w/2;
-						  }
-	//      this._super(dt);	      // no need for this call anymore
-		//}}
-		}
-	  }
-  );
+    step: function(dt) {
+      if(Q.inputs['left']) { 
+        this.p.x -= dt * this.p.speed;
+      } else if(Q.inputs['right']) {
+        this.p.x += dt * this.p.speed;
+      }
+      if(this.p.x < 0) { 
+        this.p.x = 0;
+      } else if(this.p.x > Q.width - this.p.w) { 
+        this.p.x = Q.width - this.p.w;
+      }
+//      this._super(dt);	      // no need for this call anymore
+    }
+  });
 
   Q.Sprite.extend("Ball", {
     init: function() {
@@ -119,7 +111,7 @@ $(function() {
       var blockCount=0;
       for(var x=0;x<6;x++) {
         for(var y=0;y<5;y++) {
-          stage.insert(new Q.Block({ x: x*50+35, y: y*30+10 }));
+          stage.insert(new Q.Block({ x: x*50+10, y: y*30+10 }));
           blockCount++;
         }
       }
